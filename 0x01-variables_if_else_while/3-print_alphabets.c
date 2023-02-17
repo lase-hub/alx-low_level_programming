0c1ddadabe6f617c13882e7d04fbd1caef57d6c3
lase <akeopeyemi123@gmail.com>
2023-02-17 12:49:51 -0800

    Lower to upper

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -14,12 +14,12 @@ int main(void)
 
 	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 
-	{
 		putchar(alphabet);
-		int alphabet = toupper(alphabet);
+
+	for (alphabet = 'A'; alphabet <= 'Z'; alphabet++)
 
 		putchar(alphabet);
-	}
+
 
 	putchar('\n');
 
