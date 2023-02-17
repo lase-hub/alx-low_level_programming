1e2497da59ecc89a2b8e249b51abbb8cf3869d88
lase <akeopeyemi123@gmail.com>
2023-02-17 10:51:31 -0800

    Print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - print in lowercase
+ *
+ * Description - A putchar function to print lowercase
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	char alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z' alphabet++)
+	{
+		putchar(alphabet, '\n');
+	}
+
+	return (0);
+
+}
