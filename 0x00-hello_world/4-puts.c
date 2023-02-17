fb9e290931eaede62ebd2ee3a23be39664caa2a1
lase <akeopeyemi123@gmail.com>
2023-02-17 07:12:07 -0800

    Using puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main: Writing a program that prints a line with puts function
+ * return: 0
+ */
+int main(void)
+{
+puts("Programming is like building a multilingual puzzle");
+return(o);
+}  
