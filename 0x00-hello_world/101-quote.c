237b365a2ce9603ba02db7c916e9d08fc2712417
lase <akeopeyemi123@gmail.com>
2023-02-18 17:56:41 -0800

    Standard error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,6 +6,6 @@
  */
 int main(void)
 {
-	putchar("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 30);
 	return (1);
 }
