4f0d5abfd5524d59b9e31fe338957c133d89307c
lase <akeopeyemi123@gmail.com>
2023-03-03 08:51:25 -0800

    Print numbers

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * *leet - encodes a string
+ * @str: the string
+ *
+ * Return: str
+ */
+char *leet(char *str)
+{
+	int a, b;
+	char letter[] = "aAeEoOtTlL";
+	char num[] = "43071";
+
