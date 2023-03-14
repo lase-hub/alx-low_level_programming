0db103879d83d379edecbe9f21b0a8c3022f9a4e
lase <akeopeyemi123@gmail.com>
2023-03-14 07:45:30 -0700

    Prints its name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - prints the name
+ * @argc: prints the number of arguments
+ * @argv: an array that points to argc
+ *
+ * Return: 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	argc = 0;
+
+	printf("%s\n", argv[argc]);
+	return (0);
+}
